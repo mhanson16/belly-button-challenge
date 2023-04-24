0c03d0ee04f075a78e3134896395dc181282a23a
Maddy Hanson <maddyhanson@gmail.com>
2023-04-24 15:04:42 -0700

    fixed metadata and added bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,32 +2,32 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 
-d3.json(url).then(function (data){
-//console.log(data);
+d3.json(url).then(function (data) {
+  //console.log(data);
 });
 
 
 
-function init(){
+function init() {
 
-  d3.json(url).then(function(data){
-      
-      var drop_down = d3.select("#selDataset");
-      // create dropdown menu and append
-      var name_ids = data.names;
-      name_ids.forEach((input)=>{
-        drop_down
+  d3.json(url).then(function (data) {
+
+    var drop_down = d3.select("#selDataset");
+    // create dropdown menu and append
+    var name_ids = data.names;
+    name_ids.forEach((input) => {
+      drop_down
         .append("option")
         .text(input)
         .property("value", input);
-        } );
+    });
 
-//let first ID be default
-let first_id = (data.names[0]);
-// calling other functions
-Charts(first_id);
-TotalMetadata(first_id);
-});
+    //let first ID be default
+    let first_id = (data.names[0]);
+    // calling other functions
+    Charts(first_id);
+    TotalMetadata(first_id);
+  });
 };
 
 init()
@@ -42,34 +42,63 @@ init()
 
 // function for updating all charts as metadata is selected/changed
 
-function Charts(input){
-  d3.json(url).then(function (data){
+function Charts(input) {
+  d3.json(url).then(function (data) {
     // filtering data for input
     let samples = data.samples.filter(n => n.id == input);
     // result of filtered data
     let result = samples[0];
-    
-    // variables, in decsending order then top 10
-    let sample_values = result.sample_values.slice(0,10).reverse();
-    let otu_ids = result.otu_ids.slice(0,10).map(ids => `OTU ${ids}`).reverse();
-    let otu_labels = result.otu_labels.slice(0,10).reverse();
 
+    // variables, in decsending order then top 10
+    let sample_values = result.sample_values;
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+
+    var yticks = otu_ids.slice(0, 10).map(ids => `OTU ${ids}`).reverse();
 
     // trace for bar chart
     let trace_01 = {
-      x: sample_values,
-      y: otu_ids,
-      text: otu_labels,
+      x: sample_values.slice(0, 10).reverse(),
+      y: yticks,
+      text: otu_labels.slice(0, 10).reverse(),
       type: "bar",
       orientation: "h"
     };
 
     let barChart_data = [trace_01];
     let layout = {
-      title: `Top Ten + ${input}`,
+      title: `Top 10 OTUs found in ${input}`,
     };
 
     Plotly.newPlot("bar", barChart_data, layout);
+
+    //start bubble chart
+    var bubbleChart_data = {
+      x: otu_ids,
+      y: sample_values,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Sunset"
+      }
+
+    };
+
+    var bubble_layout = {
+      title: "OTUs Based on Size",
+      xaxis: {title:"Otu ID"},
+      margin: {t:30},
+      hovermode: "closest"
+
+
+    };
+
+    Plotly.newPlot("bubble", [bubbleChart_data], bubble_layout);
+
+
+
+
   });
 
 
@@ -77,29 +106,25 @@ function Charts(input){
 
 // Updating the metadata function
 
-function TotalMetadata(input){
-  d3.json(url).then(function (data){
+function TotalMetadata(input) {
+  d3.json(url).then(function (data) {
     var metadata = data.metadata;
-    
+
     // filtering data for input
     let filtered = metadata.filter(x => x.id == input);
     // result of filtered data
     let result = filtered[0];
     console.log(result);
     // placing findings in proper location
-    var panel_input = d3.select("#select-metadata");
-    
+    var panel_input = d3.select("#sample-metadata");
+
     panel_input.html("");
-    //
-    panel_input.append("h5").text("id: " + result.id);
-    panel_input.append("h5").text("ethnicity: " + result.ethnicity);
-    panel_input.append("h5").text("geneder: " + result.gender);
-    panel_input.append("h5").text("age: " + result.age);
-    panel_input.append("h5").text("location: " + result.location);
-    panel_input.append("h5").text("bbtype: " + result.bbtype);
-    panel_input.append("h5").text("wfreq: " + result.wfreq);
+
+    Object.entries(result).forEach(([k, v]) => {
+      panel_input.append("h5").text(`${k}: ${v}`);
     });
-  };
+  });
+};
 
 
 
@@ -107,7 +132,7 @@ function TotalMetadata(input){
 
 
 // function to allow for info and charts to update when a new id is selected
-function Changed(newSample){
+function optionChanged(newSample) {
 
   //console.log(newSample);
 
